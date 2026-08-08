07b63c910d8852d41d530893644b3bd7d9523f4f
Subhankar733 <Subhankardas2569@gmail.com>
2026-08-08 20:52:52 +0530

    fix: add Material and CoordinatorLayout dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,4 +23,6 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 }
