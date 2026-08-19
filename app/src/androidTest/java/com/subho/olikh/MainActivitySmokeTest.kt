b148b241b73a6c74f61ec769aa6714b76feff6e1
Subhankar733 <Subhankardas2569@gmail.com>
2026-08-19 13:59:54 +0530

    fix: correct Espresso matches import

--- app/src/androidTest/java/com/subho/olikh/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/subho/olikh/MainActivitySmokeTest.kt
@@ -4,7 +4,7 @@ import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.matches
+import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.junit.Rule
 import org.junit.Test
